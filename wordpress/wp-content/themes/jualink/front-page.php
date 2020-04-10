e9f8fe9a1c4313322b51b0ce389972a839ac9a15
kao-ri <mt.kaorin@gmai.com>
2020-04-11 02:19:07 +0900

    [Add] WordPress テーマの設置
    オリジナルテーマ「jualink」の設置

---
+++ wordpress/wp-content/themes/jualink/front-page.php
@@ -0,0 +1,39 @@
+
+
+
+
+<main role="main">
+
+
+	<!-- FV -->
+	<article class="top-fv">
+		<div class="">
+			<p>美しく実用的なコードの探求は、<span>わたしの甘やかな楽しみです。</span></p>
+		</div>
+		<div class="">
+			<p>
+				このスライドに使用した写真は、東アフリカの国々、ケニア・タンザニアの風景。わたしが実際に渡阿したときに撮影したものです。もうずいぶんと時間が過ぎました。子供達は成長し、街の様子も変わってしまったでしょう。
+			</p>
+			<p>
+				けれど、写真を見るだけで今でも鮮やかに蘇ります。その生きた匂いまでも。
+			</p>
+			<p>
+				「アフリカの水を飲んだ者はアフリカにかえる」古いことわざの通り、わたしは何度もアフリカに通いました。海岸。雑踏。ナイロビの冷たい風。狂おしいほどにやさしく、切ないほどに懐かしい。愛し大地、アフリカ。
+			</p>
+		</div>
+	</article>
+
+	<!-- Update History -->
+	<section class="top-uphistory">
+		<h2>Update History</h2>
+		<ol class="top-uphistory__box">
+			<li class="top-uphistory__box__">
+				<p></p>
+				<div class="">
+					<p></p>
+				</div>
+			</li>
+		</ol>
+	</section>
+
+</main>
---
+++ wordpress/wp-content/themes/jualink/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ja" dir="ltr">
+<head>
+<meta charset="utf-8">
+<title>jua.Link | Webサイトのデザインからコーディングまで</title>
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<meta name="description" content="Webサイト、ホームページのデザインからコーディングまで。丁寧な仕事をしています。フロントエンドエンジニアの Kaori.sun のポートフォリオです。">
+<meta property="og:title" content="jua.Link | kaori.sun のポートフォリオ">
+<meta property="og:type" content="ポートフォリオ">
+<meta property="og:url" content="https://jua.link/">
+<meta property="og:image" content="">
+<meta property="og:site_name" content="jua.Link">
+<meta property="og:description" content="Webサイト、ホームページのデザインからコーディングまで。丁寧な仕事をしています。フロントエンドエンジニアの Kaori.sun のポートフォリオです。">
+<link rel="stylesheet" href="./style.css">
+</head>
+
+<body>
+	
+<header>
+
+</header>
+
+</body>
+</html>
